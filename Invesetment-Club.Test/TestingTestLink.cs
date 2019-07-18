b78ca4f5fa779f2466f2e4266c9939c4d6c86fdf
WaysideTester <dylanmurray211@outlook.com>
2019-07-17 21:08:50 -0500

    successfully connected test with solution

---
+++ Invesetment-Club.Test/TestingTestLink.cs
@@ -0,0 +1,23 @@
+using Pages;
+using System;
+using Xunit;
+
+namespace Invesetment_Club.Test
+{
+    public class TestingTestLink
+    {
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 1, 3)]
+        [InlineData(2, 2, 4)]
+        public void Check_Single_Addition_Works(int firstNumber, int secondNumber, int expectedResult)
+        {
+            TestLink testLinkEngine = new TestLink();
+
+            var result = testLinkEngine.SimpleAddition(firstNumber, secondNumber);
+
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
---
+++ Investment-Clubs/Pages/TestLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pages
+{
+    public class TestLink
+    {
+        public int SimpleAddition(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+    }
+}
