7496c0125b3fcd800e1b67fa7fb0ca122f57b819
WaysideTester <dylanmurray211@outlook.com>
2019-07-17 20:22:36 -0500

    fixed but where App.js wasnt linked

---
+++ Investment-Clubs/ClientApp/src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+  displayName = App.name
+
+  render() {
+    return (
+        <div>
+          <p>App is linked</p>
+        </div>
+    );
+  }
+}
