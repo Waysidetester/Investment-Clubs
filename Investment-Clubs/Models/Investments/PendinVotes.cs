0dfc23ae8b9543f436bd606d161820d778a8916c
WaysideTester <dylanmurray211@outlook.com>
2019-07-20 17:50:39 -0500

    setting up query

---
+++ Investment-Clubs/Models/Investments/PendinVotes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Investment_Clubs.Models.Investments
+{
+    public class PendingVotes : IInvestment
+    {
+        public bool? Vote { get; set; }
+        public bool Abstain { get; set; }
+        public string ReceivingEntity { get; set; }
+        public string ClubName { get; set; }
+        public string InvestmentType { get; set; }
+    }
+}
