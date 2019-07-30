e35f872a2eec81c58db729e02659b05af547b45e
WaysideTester <dylanmurray211@outlook.com>
2019-07-29 21:14:43 -0500

    successfully displaying data on modal

--- Investment-Clubs/ClientApp/src/SiteWide/Modal/InvestmentModal/InvestmentModal.js
+++ Investment-Clubs/ClientApp/src/SiteWide/Modal/InvestmentModal/InvestmentModal.js
@@ -4,22 +4,50 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  ModalFooter
+  ModalFooter,
+  Spinner
 } from 'reactstrap';
 import './InvestmentModal.scss';
 
 class InvestmentModal extends React.Component{
+  valCheck = (text, value) => {
+    if(value !== null){
+      return <p>{text}{value}</p>
+    }
+  }
+
   render(){
+    if(this.props.InvDetail !== null){
+      return(
+          <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
+            <ModalHeader toggle={this.props.toggle}>{this.props.InvDetail.receivingEntity}</ModalHeader>
+            <ModalBody>
+              <p>Investment Type: {this.props.InvDetail.investmentType}</p>
+              {this.valCheck('Club Investment Amount: ',this.props.InvDetail.dollarsInvested)}
+              {this.valCheck('Bond Coupon: ',this.props.InvDetail.debtCoupon)}
+              {this.valCheck('Asset Units Purchased: ',this.props.InvDetail.ownershipUnits)}
+              {this.valCheck('Convertable: ',(this.props.InvDetail.convertable ? 'yes':'no'))}
+              {this.valCheck('Date Bond Matures: ',this.props.InvDetail.matureDate)}
+              {this.valCheck('Equity Bought: ',this.props.InvDetail.percentEquity)}
+              {this.valCheck('Date Invested: ',this.props.InvDetail.investDate)}
+            </ModalBody>
+            <ModalFooter>
+              <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+      );
+    }
+
     return(
-        <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.props.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+      <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
+        <ModalHeader toggle={this.props.toggle}><Spinner color='info' /></ModalHeader>
+        <ModalBody>
+          <Spinner color='info' />
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
     );
   }
 }
